7e0d7209b48ba6979d941490ef45ff00dde7e5a6
Mikołaj Kowalski <cmosqt@gmail.com>
2021-02-28 00:25:20 +0100

    provider params

--- providers.tf
+++ providers.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "esxi" {
-  esxi_hostname = "esxi"
-  esxi_hostport = "22"
-  esxi_hostssl  = "443"
-  esxi_username = "root"
-  esxi_password = ""
+  esxi_hostname = var.esxi_hostname
+  esxi_hostport = var.esxi_hostport
+  esxi_hostssl  = var.esxi_hostssl
+  esxi_username = var.esxi_username
+  esxi_password = var.esxi_password
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "esxi_hostname" { default = "esxi" }
+variable "esxi_hostport" { default = "22" }
+variable "esxi_username" { default = "root" }
+variable "esxi_password" {} # Unspecified will prompt
